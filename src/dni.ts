7f96febf0a53f800af010622a9473eca204a6612
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-24 10:52:31 +0100

    refactor: add constraint readonly in attributes. Remove unnecessary check. And create methods equals and toString

--- src/dni.ts
+++ src/dni.ts
@@ -3,17 +3,13 @@ export class Dni {
 
     static INVALID_LETTERS = ["U", "I", "O", "Ñ"]
 
-    private code: number
+    private readonly code: number
 
-    private letter: string
+    private readonly letter: string
 
     constructor(value: string) {
-        if(value.length != 9) {
-            throw new Error("DNI " + value + " has a wrong length")
-        }
-
-        if(!Dni.PATTERN.test(value)) {
-            throw new Error("DNI " + value + " has a wrong format")
+        if (!Dni.PATTERN.test(value)) {
+            throw new Error(`DNI ${value} has a wrong format`)
         }
 
         const codeStr = value.slice(0, 8)
@@ -21,8 +17,21 @@ export class Dni {
 
         this.letter = value.slice(-1)
 
-        if(Dni.INVALID_LETTERS.includes(this.letter)) {
-            throw new Error("DNI " + value + " has an invalid letter: " + Dni.INVALID_LETTERS)
+        if (Dni.INVALID_LETTERS.includes(this.letter)) {
+            throw new Error(`DNI ${value} has an invalid letter: ${Dni.INVALID_LETTERS}`)
         }
+    }
+
+    equals(other: any): boolean {
+        if (!(other instanceof Dni)) {
+            return false
+        }
+
+        return this.code === other.code
+            && this.letter === other.letter;
+    }
+
+    toString(): string {
+        return `${this.code}${this.letter}`
     }
 }
