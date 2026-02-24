a4ab34b968d842384bdf1f6939b2abd59bf76540
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-24 10:39:40 +0100

    feature: check format of dni: 8 numbers + 1 character

--- src/dni.test.ts
+++ src/dni.test.ts
@@ -16,4 +16,12 @@ describe('DNI', () => {
     it('should fail because length is less than 9 characters', () => {
         expect(() => new Dni("045607322Q")).toThrow()
     })
+
+    it('should fail because there is a character in first positions', () => {
+        expect(() => new Dni("A4560732Q")).toThrow()
+    })
+
+    it('should fail because last position is a number', () => {
+        expect(() => new Dni("045607321")).toThrow()
+    })
 })
--- src/dni.ts
+++ src/dni.ts
@@ -1,7 +1,13 @@
 export class Dni {
-    constructor(private value: String) {
+    static PATTERN = /^\d{8}[a-zA-Z]$/;
+
+    constructor(private value: string) {
         if(value.length != 9) {
             throw new Error("DNI " + value + " has a wrong length");
+        }
+
+        if(!Dni.PATTERN.test(value)) {
+            throw new Error("DNI " + value + " has a wrong format");
         }
     }
 }
