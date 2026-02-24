d3e29b6ba833553eac53ddc83de85ceb40e27220
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-24 10:44:16 +0100

    refactor: remove attribute value and save data in new attributes code and letter

--- src/dni.ts
+++ src/dni.ts
@@ -1,13 +1,22 @@
 export class Dni {
-    static PATTERN = /^\d{8}[a-zA-Z]$/;
+    static PATTERN = /^\d{8}[a-zA-Z]$/
 
-    constructor(private value: string) {
+    private code: number
+
+    private letter: string
+
+    constructor(value: string) {
         if(value.length != 9) {
-            throw new Error("DNI " + value + " has a wrong length");
+            throw new Error("DNI " + value + " has a wrong length")
         }
 
         if(!Dni.PATTERN.test(value)) {
-            throw new Error("DNI " + value + " has a wrong format");
+            throw new Error("DNI " + value + " has a wrong format")
         }
+
+        const codeStr = value.slice(0, 8)
+        this.code = Number(codeStr)
+
+        this.letter = value.slice(-1)
     }
 }
