427412000bf745f4c94cf49f6e07434c33bf2593
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-25 10:01:53 +0100

    fix: circular dependency

--- src/data_retriever/DataMarketStudyRetriever.ts
+++ src/data_retriever/DataMarketStudyRetriever.ts
@@ -1,7 +1,8 @@
 import {DataRetriever} from "./DataRetriever";
+import {MarketStudyVendor} from "../../lib/MarketStudyVendor";
 
 export class DataMarketStudyRetriever implements DataRetriever {
-    private marketStudyRetriever = new DataMarketStudyRetriever()
+    private marketStudyRetriever = new MarketStudyVendor()
 
     averagePrice(blog: string): number {
         return this.marketStudyRetriever.averagePrice(blog)
