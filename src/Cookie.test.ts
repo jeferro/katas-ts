97ba195534298e3176f1a6be38f57857b8bbcf8c
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-25 22:21:09 +0100

    feature: add test "cookie with chocolate"

--- src/Cookie.test.ts
+++ src/Cookie.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest'
 import {Cookie} from "./Cookie"
+import {Chocolate} from "./toppings/Chocolate"
 
 describe('Cookie', () => {
 
@@ -7,5 +8,12 @@ describe('Cookie', () => {
         const cake = new Cookie()
 
         expect(cake.name).toBe('Cookie')
+    })
+
+    it('should return name "Cookie with chocolate"', () => {
+        const cookie = new Cookie()
+        const cake = new Chocolate(cookie)
+
+        expect(cake.name).toBe('Cookie with chocolate')
     })
 })
