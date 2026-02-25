4e51579ce8cc5a2ad6731c41df5537d1e52c22b2
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-25 16:53:06 +0100

    feature: allow increase other cases

--- src/ScoreKeeper.ts
+++ src/ScoreKeeper.ts
@@ -7,6 +7,25 @@ export class ScoreKeeper {
         this.scoreTeamA += 1
     }
 
+    scoreTeamA2() {
+        this.scoreTeamA += 2
+    }
+
+    scoreTeamA3() {
+        this.scoreTeamA += 3
+    }
+    scoreTeamB1() {
+        this.scoreTeamB += 1
+    }
+
+    scoreTeamB2() {
+        this.scoreTeamB += 2
+    }
+
+    scoreTeamB3() {
+        this.scoreTeamB += 3
+    }
+
     getScore(): string {
         const scoreTeamAStr = this.scoreTeamA.toString().padStart(3, '0')
         const scoreTeamBStr = this.scoreTeamB.toString().padStart(3, '0')
