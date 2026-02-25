c5e801c20cb99202e0c469fd5b555ae6c5da6537
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-25 20:15:19 +0100

    refactor: use variable previous frame to simplify code

--- src/BowlingPlayer.ts
+++ src/BowlingPlayer.ts
@@ -8,9 +8,12 @@ export class BowlingPlayer {
     score(attempt1: number, attempt2: number) {
         this._frames[this._currentFrame] = Frame.create(attempt1, attempt2)
 
-        if(this._frames.length > 1
-            && this._frames[this._currentFrame - 1].isSpare) {
-            this._frames[this._currentFrame - 1].scoreSpare(attempt1)
+        const previousFrame = this._currentFrame > 0 ?
+            this._frames[this._currentFrame - 1]
+            : undefined
+
+        if(previousFrame && previousFrame.isSpare){
+            previousFrame.scoreSpare(attempt1)
         }
 
         this._currentFrame += 1
