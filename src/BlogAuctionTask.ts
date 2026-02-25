83b8f48a3038b7790cc2585760bc80fde6a01c3d
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-25 10:19:34 +0100

    refactor: clean code in BlogAuctionTask

--- src/BlogAuctionTask.ts
+++ src/BlogAuctionTask.ts
@@ -1,6 +1,7 @@
 import {ProposalPublisher} from "./proposal_publisher/ProposalPublisher";
 import {DataRetriever} from "./data_retriever/DataRetriever";
 import {TimeService} from "./timeservice/TimeService";
+import {NumberUtils} from "./utils/NumberUtils";
 
 export class BlogAuctionTask {
     constructor(private dataRetriever: DataRetriever,
@@ -9,39 +10,45 @@ export class BlogAuctionTask {
     }
 
     priceAndPublish(blog: string, mode: string) {
-        const avgPrice = this.dataRetriever.averagePrice(blog)
+        let timeFactor = this.calculateTimeFactor(mode);
 
+        let proposal = this.calculateProposal(blog, timeFactor);
+
+        this.proposalPublisher.publish(proposal)
+    }
+
+    private calculateProposal(blog: string, timeFactor: number): number {
         // FIXME should actually be +2 not +1
+        let proposal = this.dataRetriever.averagePrice(blog) + 1
 
-        let proposal = avgPrice + 1
-        let timeFactor = 1
+        if (NumberUtils.isOdd(proposal)) {
+            return 3.14 * proposal
+        }
 
+        const now = this.timeService.now()
+        const lastDayOfPastYear = this.timeService.lastDayOfPastYear()
+        const diffTime = now.getTime() - lastDayOfPastYear.getTime()
+
+        return 3.15 * timeFactor * Math.round(diffTime / 10000000)
+    }
+
+    private calculateTimeFactor(mode: string): number {
         if (mode === 'SLOW') {
-            timeFactor = 2
+            return 2
         }
 
         if (mode === 'MEDIUM') {
-            timeFactor = 4
+            return 4
         }
 
         if (mode === 'FAST') {
-            timeFactor = 8
+            return 8
         }
 
         if (mode === 'ULTRAFAST') {
-            timeFactor = 13
+            return 13
         }
 
-        if (proposal % 2 === 0) {
-            proposal = 3.14 * proposal
-        } else {
-            const now = this.timeService.now()
-            const lastDayOfPastYear = this.timeService.lastDayOfPastYear()
-            const diffTime = now.getTime() - lastDayOfPastYear.getTime()
-
-            proposal = 3.15 * timeFactor * Math.round(diffTime / 10000000)
-        }
-
-        this.proposalPublisher.publish(proposal)
+        return 1
     }
 }
