140669b5c92f18af1466571721811fc4d1daa416
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-25 12:43:53 +0100

    refactor: create util class DateUtils and move method numYearBetween

--- src/Contract.ts
+++ src/Contract.ts
@@ -1,3 +1,5 @@
+import {DateUtils} from "./DateUtils";
+
 export class Contract {
 
     constructor(private readonly name: string,
@@ -10,18 +12,6 @@ export class Contract {
             return (testDate.getMonth() + 1) * 2
         }
 
-        return 24 + this.numOfYearsInEnterprise(testDate)
-    }
-
-    private numOfYearsInEnterprise(testDate: Date) {
-        let years = testDate.getFullYear() - this.startDate.getFullYear()
-        const months = testDate.getMonth() - this.startDate.getMonth()
-
-        if (months < 0
-            || (months === 0 && testDate.getDate() < this.startDate.getDate())) {
-            years--;
-        }
-
-        return years;
+        return 24 + DateUtils.numYearBetween(testDate, this.startDate)
     }
 }
