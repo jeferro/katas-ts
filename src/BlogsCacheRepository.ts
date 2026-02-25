181679180c0a8c932808edc169d7b837dcc88a56
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-25 10:44:19 +0100

    refactor: apply guard clause in method BlogsCacheRepository.findAll

--- src/BlogsCacheRepository.ts
+++ src/BlogsCacheRepository.ts
@@ -1,16 +1,16 @@
 import {TechBlogs} from "./TechBlogs"
 
 export class BlogsCacheRepository {
+
     private static cache: string[] = []
 
     static findAll(): string[] {
-        if (this.cache.length > 0) {
-            return this.cache
-        }
-
         // FIXME : only return blogs that start with a 'T'
 
-        this.cache = TechBlogs.listAllBlogs()
+        if (this.cache.length == 0) {
+            this.cache = TechBlogs.listAllBlogs()
+        }
+
         return this.cache
     }
 }
