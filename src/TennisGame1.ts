4f39a959eff1dfa16c2306c1cacb77c988e6a260
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-25 13:51:11 +0100

    refactor: TennisGame1 :: format code

--- src/TennisGame1.ts
+++ src/TennisGame1.ts
@@ -1,59 +1,59 @@
-import { TennisGame } from './TennisGame'
+import {TennisGame} from './TennisGame'
 
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0
-  private m_score2: number = 0
+    private m_score1: number = 0
+    private m_score2: number = 0
 
-  constructor(private readonly player1Name: string,
-              private readonly player2Name: string) {
-  }
-
-  wonPoint(playerName: string): void {
-    if (playerName === 'player1')
-      this.m_score1 += 1
-    else
-      this.m_score2 += 1
-  }
-
-  getScore(): string {
-    if (this.playersAreTied()) {
-      if(this.m_score1 < 3){
-        return `${this.mapScoreToDescription(this.m_score1)}-All`
-      }
-
-        return 'Deuce'
+    constructor(private readonly player1Name: string,
+                private readonly player2Name: string) {
     }
 
-    if (this.somePlayerHasMore3Points()) {
-      const scoreDif = Math.abs(this.m_score1 - this.m_score2)
-      const playerName = this.m_score1 > this.m_score2 ? 'player1' : 'player2'
-
-      return scoreDif === 1 ? `Advantage ${playerName}` : `Win for ${playerName}`
+    wonPoint(playerName: string): void {
+        if (playerName === 'player1')
+            this.m_score1 += 1
+        else
+            this.m_score2 += 1
     }
 
-    return `${this.mapScoreToDescription(this.m_score1)}-${this.mapScoreToDescription(this.m_score2)}`
-  }
+    getScore(): string {
+        if (this.playersAreTied()) {
+            if (this.m_score1 < 3) {
+                return `${this.mapScoreToDescription(this.m_score1)}-All`
+            }
 
-  private somePlayerHasMore3Points() {
-    return this.m_score1 >= 4 || this.m_score2 >= 4
-  }
+            return 'Deuce'
+        }
 
-  private playersAreTied() {
-    return this.m_score1 === this.m_score2
-  }
+        if (this.somePlayerHasMore3Points()) {
+            const scoreDif = Math.abs(this.m_score1 - this.m_score2)
+            const playerName = this.m_score1 > this.m_score2 ? 'player1' : 'player2'
 
-  private mapScoreToDescription(tempScore: number) {
-    switch (tempScore) {
-      case 0:
-        return 'Love'
-      case 1:
-        return 'Fifteen'
-      case 2:
-        return 'Thirty'
-      case 3:
-        return 'Forty'
-      default:
-        throw new Error(`Unrecognized score: ${tempScore}`)
+            return scoreDif === 1 ? `Advantage ${playerName}` : `Win for ${playerName}`
+        }
+
+        return `${this.mapScoreToDescription(this.m_score1)}-${this.mapScoreToDescription(this.m_score2)}`
     }
-  }
+
+    private somePlayerHasMore3Points() {
+        return this.m_score1 > 3 || this.m_score2 > 3
+    }
+
+    private playersAreTied() {
+        return this.m_score1 === this.m_score2
+    }
+
+    private mapScoreToDescription(tempScore: number) {
+        switch (tempScore) {
+            case 0:
+                return 'Love'
+            case 1:
+                return 'Fifteen'
+            case 2:
+                return 'Thirty'
+            case 3:
+                return 'Forty'
+            default:
+                throw new Error(`Unrecognized score: ${tempScore}`)
+        }
+    }
 }
