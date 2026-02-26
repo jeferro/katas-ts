95756f4b542979cfb5c4eabf99815c23fb2e20f5
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-26 09:32:16 +0100

    refactor: clean code of method loadUsersFromWeb

--- src/ImportUserScript.ts
+++ src/ImportUserScript.ts
@@ -27,31 +27,29 @@ export class ImportUserScript {
     }
 
     private async loadUsersFromWeb() {
-        /** This kata uses "fetch()", be aware you need at least Node 18 to run the script */
-        var USER_URL = 'https://randomuser.me/api/?inc=gender,name,email,location&results=5&seed=a9b25cd955e2037h';
+        const url = 'https://randomuser.me/api/?inc=gender,name,email,location&results=5&seed=a9b25cd955e2037h'
 
-        // Parse URL content
-        let url = USER_URL
+        const response = await fetch(url);
+        const responseJson = await response.json();
 
-        var web_provider = (await (await fetch(url)).json()).results;
+        let userId = 1_00_000_000_000.51;
+        const userBirthDay = new Date().getFullYear()
 
-        var b = []
-        var i = 1_00_000_000_000.51
-        for (let j = 0; j < web_provider.length; j++) {
-            i++
-            if (web_provider[j] instanceof Object) {
-                b.push([
-                    parseInt(i.toString()), // id
-                    web_provider[j].gender,
-                    web_provider[j].name.first + ' ' + web_provider[j].name.last,
-                    web_provider[j].location.country,
-                    web_provider[j].location.postcode,
-                    web_provider[j].email,
-                    new Date().getFullYear() // birhtday
-                ])
-            }
-        }
-        return b;
+        return responseJson.results
+            .filter((result: any) => result instanceof Object)
+            .map((result: any) => {
+                userId++
+
+                return [
+                    parseInt(userId.toString()),
+                    result.gender,
+                    result.name.first + ' ' + result.name.last,
+                    result.location.country,
+                    result.location.postcode,
+                    result.email,
+                    userBirthDay
+                ]
+            })
     }
 
     private loadUsersFromCsv() {
