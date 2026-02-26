7dd96e9df5d92966ec0abc126901b3ae5e5bdf9c
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-26 10:48:50 +0100

    refactor: change visibility of private methods and properties to protected

--- src/Parrot.ts
+++ src/Parrot.ts
@@ -6,10 +6,10 @@ export enum ParrotTypes {
 }
 
 export class Parrot {
-    constructor(private parrotType: ParrotTypes,
-                private numberOfCoconuts: number,
-                private voltage: number,
-                private isNailed: boolean) {
+    constructor(protected parrotType: ParrotTypes,
+                protected numberOfCoconuts: number,
+                protected voltage: number,
+                protected isNailed: boolean) {
     }
 
     public getSpeed(): number {
@@ -24,15 +24,15 @@ export class Parrot {
         throw new Error("Should be unreachable");
     }
 
-    private getBaseSpeed(): number {
+    protected getBaseSpeed(): number {
         return 12;
     }
 
-    private getLoadFactor(): number {
+    protected getLoadFactor(): number {
         return 9;
     }
 
-    private getBaseSpeedWithVoltage(voltage: number): number {
+    protected getBaseSpeedWithVoltage(voltage: number): number {
         return Math.min(24, voltage * this.getBaseSpeed());
     }
 
