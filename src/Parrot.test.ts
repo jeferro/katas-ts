9e38bfc92f21cb389e164321e6c9c89e0f2619a6
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-26 11:18:06 +0100

    refactor: remove unused parameters in factory methods

--- src/Parrot.test.ts
+++ src/Parrot.test.ts
@@ -1,61 +1,60 @@
 import { describe, it, expect } from 'vitest'
 
-import {ParrotTypes} from '../src/parrot'
 import {ParrotFactory} from "./ParrotFactory";
 
 describe('Parrot', () => {
 
     it('gets speed of European Parrot', () => {
-        const parrot = ParrotFactory.createEuropean(ParrotTypes.EUROPEAN, 0, 0, false);
+        const parrot = ParrotFactory.createEuropean();
         expect(parrot.getSpeed()).toBe(12);
     });
 
     it('gets speed of African Parrot with one coconut', () => {
-        const parrot = ParrotFactory.createAfrican(ParrotTypes.AFRICAN, 1, 0, false);
+        const parrot = ParrotFactory.createAfrican(1);
         expect(parrot.getSpeed()).toBe(3);
     });
 
     it('gets speed of African Parrot with two coconuts', () => {
-        const parrot = ParrotFactory.createAfrican(ParrotTypes.AFRICAN, 2, 0, false);
+        const parrot = ParrotFactory.createAfrican(2);
         expect(parrot.getSpeed()).toBe(0);
     });
 
     it('gets speed of African Parrot with no coconuts', () => {
-        const parrot = ParrotFactory.createAfrican(ParrotTypes.AFRICAN, 0, 0, false);
+        const parrot = ParrotFactory.createAfrican(0);
         expect(parrot.getSpeed()).toBe(12);
     });
 
     it('gets speed of Norwegian Blue Parrot nailed', () => {
-        const parrot = ParrotFactory.createNorwegianBlue(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, true);
+        const parrot = ParrotFactory.createNorwegianBlue(1.5, true);
         expect(parrot.getSpeed()).toBe(0);
     });
 
     it('gets speed of Norwegian Blue Parrot not nailed', () => {
-        const parrot = ParrotFactory.createNorwegianBlue(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, false);
+        const parrot = ParrotFactory.createNorwegianBlue(1.5, false);
         expect(parrot.getSpeed()).toBe(18);
     });
 
     it('gets speed of Norwegian Blue Parrot not nailed high voltage', () => {
-        const parrot = ParrotFactory.createNorwegianBlue(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+        const parrot = ParrotFactory.createNorwegianBlue(4, false);
         expect(parrot.getSpeed()).toBe(24);
     });
 
     it('gets cry of European Parrot', () => {
-        const parrot = ParrotFactory.createEuropean(ParrotTypes.EUROPEAN, 0, 0, false);
+        const parrot = ParrotFactory.createEuropean();
         expect( parrot.getCry()).toBe("Sqoork!");
     });
 
     it('gets cry of African Parrot', () => {
-        const parrot = ParrotFactory.createAfrican(ParrotTypes.AFRICAN, 1, 0, false);
+        const parrot = ParrotFactory.createAfrican(1);
         expect( parrot.getCry()).toBe("Sqaark!");
     });
     it('gets cry of Norwegian Blue with high voltage', () => {
-        const parrot = ParrotFactory.createNorwegianBlue(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+        const parrot = ParrotFactory.createNorwegianBlue(4, false);
         expect( parrot.getCry()).toBe("Bzzzzzz");
     });
 
     it('gets cry of NorwegianBlue without voltage', () => {
-        const parrot = ParrotFactory.createNorwegianBlue(ParrotTypes.NORWEGIAN_BLUE, 0, 0, false);
+        const parrot = ParrotFactory.createNorwegianBlue(0, false);
         expect(parrot.getCry()).toBe("...");
     });
 
--- src/ParrotFactory.ts
+++ src/ParrotFactory.ts
@@ -1,43 +1,19 @@
-import {Parrot, ParrotTypes} from "./Parrot";
+import {Parrot} from "./Parrot";
 import {AfricanParrot} from "./AfricanParrot";
 import {NorwegianBlueParrot} from "./NorwegianBlueParrot";
 import {EuropeanParrot} from "./EuropeanParrot";
 
 export abstract class ParrotFactory {
-
-    public static create(parrotType: ParrotTypes,
-                         numberOfCoconuts: number,
-                         voltage: number,
-                         isNailed: boolean): Parrot {
-        switch(parrotType) {
-            case ParrotTypes.AFRICAN:
-                return new AfricanParrot( numberOfCoconuts)
-            case ParrotTypes.NORWEGIAN_BLUE:
-                return new NorwegianBlueParrot( voltage, isNailed)
-            case ParrotTypes.EUROPEAN:
-                return new EuropeanParrot()
-        }
-
-    }
-
-    public static createAfrican(parrotType: ParrotTypes,
-                                numberOfCoconuts: number,
-                                voltage: number,
-                                isNailed: boolean): Parrot {
+    public static createAfrican(numberOfCoconuts: number): Parrot {
         return new AfricanParrot( numberOfCoconuts)
     }
 
-    public static createNorwegianBlue(parrotType: ParrotTypes,
-                                      numberOfCoconuts: number,
-                                      voltage: number,
+    public static createNorwegianBlue(voltage: number,
                                       isNailed: boolean): Parrot {
         return new NorwegianBlueParrot( voltage, isNailed)
     }
 
-    public static createEuropean(parrotType: ParrotTypes,
-                                      numberOfCoconuts: number,
-                                      voltage: number,
-                                      isNailed: boolean): Parrot {
+    public static createEuropean(): Parrot {
         return new EuropeanParrot()
     }
 }
