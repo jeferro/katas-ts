a1831abe608cb095f4c9417c53e893de1f9c259c
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-26 11:13:41 +0100

    fix: define abstract methods getSpeed and getCry in class Parrot

--- src/Parrot.ts
+++ src/Parrot.ts
@@ -7,5 +7,9 @@ export enum ParrotTypes {
 
 export abstract class Parrot {
     protected readonly BASE_SPEED = 12;
+
+    public abstract getSpeed(): number
+
+    public abstract getCry(): String
 }
 
