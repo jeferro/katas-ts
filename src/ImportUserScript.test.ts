d18ae11cc2e4f67e3024a83f30c485cbd3b36813
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-26 07:50:25 +0100

    refactor: move script to class ImportUserScript

---
+++ src/ImportUserScript.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest'
+import {ImportUserScript} from "./ImportUserScript";
+
+describe('ImportUserScript', () => {
+
+    it('should execute script', () => {
+        const script = new ImportUserScript()
+    })
+})
