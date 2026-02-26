700a506b057e5b98e1c6ed71c5260929be0bb539
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-26 11:05:08 +0100

    refactor:convert method getLocalFactor to class constant

--- src/AfricanParrot.ts
+++ src/AfricanParrot.ts
@@ -1,19 +1,17 @@
-import {Parrot, ParrotTypes} from "./Parrot";
+import {Parrot} from "./Parrot";
 
 export class AfricanParrot extends Parrot {
+    private readonly LOCAL_FACTOR = 9;
+
     constructor(numberOfCoconuts: number, voltage: number, isNailed: boolean) {
         super(numberOfCoconuts, voltage, isNailed);
     }
 
     public getSpeed(): number {
-        return Math.max(0, this.getBaseSpeed() - this.getLoadFactor() * this.numberOfCoconuts);
+        return Math.max(0, this.getBaseSpeed() - this.LOCAL_FACTOR * this.numberOfCoconuts);
     }
 
     public getCry(): String {
         return "Sqaark!";
-    }
-
-    private getLoadFactor(): number {
-        return 9;
     }
 }
