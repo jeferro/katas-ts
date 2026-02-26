a0d1b5177b2e0032511c19e53d3a7f21a46d1edf
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-02-26 10:53:36 +0100

    refactor: clone methods getSpeed and getCry in all subclasses

--- src/EuropeanParrot.ts
+++ src/EuropeanParrot.ts
@@ -4,4 +4,28 @@ export class EuropeanParrot extends Parrot {
     constructor(parrotType: ParrotTypes, numberOfCoconuts: number, voltage: number, isNailed: boolean) {
         super(ParrotTypes.EUROPEAN, numberOfCoconuts, voltage, isNailed);
     }
+
+    public getSpeed(): number {
+        switch (this.parrotType) {
+            case ParrotTypes.EUROPEAN:
+                return this.getBaseSpeed();
+            case ParrotTypes.AFRICAN:
+                return Math.max(0, this.getBaseSpeed() - this.getLoadFactor() * this.numberOfCoconuts);
+            case ParrotTypes.NORWEGIAN_BLUE:
+                return (this.isNailed) ? 0 : this.getBaseSpeedWithVoltage(this.voltage);
+        }
+        throw new Error("Should be unreachable");
+    }
+
+    public getCry(): String {
+        switch (this.parrotType) {
+            case ParrotTypes.EUROPEAN:
+                return "Sqoork!";
+            case ParrotTypes.AFRICAN:
+                return "Sqaark!";
+            case ParrotTypes.NORWEGIAN_BLUE:
+                return this.voltage > 0 ? "Bzzzzzz" : "...";
+        }
+        throw new Error("Should be unreachable");
+    }
 }
--- src/Parrot.ts
+++ src/Parrot.ts
@@ -12,18 +12,6 @@ export abstract class Parrot {
                 protected isNailed: boolean) {
     }
 
-    public getSpeed(): number {
-        switch (this.parrotType) {
-            case ParrotTypes.EUROPEAN:
-                return this.getBaseSpeed();
-            case ParrotTypes.AFRICAN:
-                return Math.max(0, this.getBaseSpeed() - this.getLoadFactor() * this.numberOfCoconuts);
-            case ParrotTypes.NORWEGIAN_BLUE:
-                return (this.isNailed) ? 0 : this.getBaseSpeedWithVoltage(this.voltage);
-        }
-        throw new Error("Should be unreachable");
-    }
-
     protected getBaseSpeed(): number {
         return 12;
     }
@@ -34,18 +22,6 @@ export abstract class Parrot {
 
     protected getBaseSpeedWithVoltage(voltage: number): number {
         return Math.min(24, voltage * this.getBaseSpeed());
-    }
-
-    public getCry(): String {
-        switch (this.parrotType) {
-            case ParrotTypes.EUROPEAN:
-                return "Sqoork!";
-            case ParrotTypes.AFRICAN:
-                return "Sqaark!";
-            case ParrotTypes.NORWEGIAN_BLUE:
-                return this.voltage > 0 ? "Bzzzzzz" : "...";
-        }
-        throw new Error("Should be unreachable");
     }
 
 }
