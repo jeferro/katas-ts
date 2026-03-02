2068d8058c70e03c271ca82352628680a7199c5a
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-02 15:56:27 +0100

    refactor: remove dead code in method update in AgedBrie

--- src/items/AgedBrie.ts
+++ src/items/AgedBrie.ts
@@ -8,34 +8,14 @@ export class AgedBrie extends Item {
   }
 
   update(): void {
-    if (this.name === 'Sulfuras, Hand of Ragnaros') {
-      return;
-    }
-
     if (this.quality < 50) {
-
       this.quality = this.quality + 1
-
-      if (this.name == 'Backstage passes to a TAFKAL80ETC concert'
-          && this.quality < 50) {
-
-        const qualityDiff = this.sellIn < 6 ? 2 : 1
-
-        this.quality = this.quality + qualityDiff
-
-      }
     }
 
-    this.sellIn = this.sellIn - 1;
+    this.sellIn = this.sellIn - 1
 
-    if (this.sellIn < 0) {
-      if (this.name == 'Aged Brie') {
-        if (this.quality < 50) {
-          this.quality = this.quality + 1
-        }
-      } else if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-        this.quality = 0
-      }
+    if (this.sellIn < 0 && this.quality < 50) {
+      this.quality = this.quality + 1
     }
   }
 
