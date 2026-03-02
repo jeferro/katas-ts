26929551d4ed5fb9e9dbddf6161c1d1837d0909f
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-02 15:59:48 +0100

    refactor: decrement sellIn in the beginning

--- src/items/AgedBrie.ts
+++ src/items/AgedBrie.ts
@@ -8,14 +8,14 @@ export class AgedBrie extends Item {
   }
 
   update(): void {
-    if (this.quality < 50) {
-      this.quality = this.quality + 1
-    }
-
     this.sellIn = this.sellIn - 1
 
-    if (this.sellIn < 0 && this.quality < 50) {
+    if (this.quality < 50) {
       this.quality = this.quality + 1
+
+      if (this.sellIn < 0 && this.quality < 50) {
+        this.quality = this.quality + 1
+      }
     }
   }
 
--- src/items/BackstagePasses.ts
+++ src/items/BackstagePasses.ts
@@ -8,24 +8,23 @@ export class BackstagePasses extends Item {
   }
 
   update(): void {
-
-    if (this.quality < 50) {
-
-      this.quality = this.quality + 1
-
-      if (this.quality < 50) {
-
-        const qualityDiff = this.sellIn < 6 ? 2 : 1
-
-        this.quality = this.quality + qualityDiff
-
-      }
-    }
-
     this.sellIn = this.sellIn - 1;
 
     if (this.sellIn < 0) {
       this.quality = 0
+      return;
+    }
+
+    if (this.quality < 50) {
+      this.quality = this.quality + 1
+
+      if (this.quality < 50) {
+
+        const qualityDiff = this.sellIn < 7 ? 2 : 1
+
+        this.quality = this.quality + qualityDiff
+
+      }
     }
   }
 }
