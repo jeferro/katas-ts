a77792d84ecfac15e5d3a7f9ac1fb379297d801e
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-02 15:23:59 +0100

    refactor: format code

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -26,14 +26,14 @@ export class GildedRose {
         this.items[i].quality = this.items[i].quality + 1
 
         if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
-          && this.items[i].quality < 50) {
+            && this.items[i].quality < 50) {
           if (this.items[i].sellIn < 11) {
 
-              this.items[i].quality = this.items[i].quality + 1
+            this.items[i].quality = this.items[i].quality + 1
 
           }
           if (this.items[i].sellIn < 6) {
-              this.items[i].quality = this.items[i].quality + 1
+            this.items[i].quality = this.items[i].quality + 1
           }
         }
       }
@@ -48,8 +48,7 @@ export class GildedRose {
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1
           }
-        }
-        else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
           this.items[i].quality = this.items[i].quality - this.items[i].quality
         }
 
