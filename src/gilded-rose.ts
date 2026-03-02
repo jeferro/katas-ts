0f66b8e104055afab38e835f92a2d664f9729845
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-02 15:36:09 +0100

    refactor: create method update in class Item

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -8,6 +8,38 @@ export class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  update(): void {
+    if (this.name === 'Sulfuras, Hand of Ragnaros') {
+      return;
+    }
+
+    if (this.quality < 50) {
+
+      this.quality = this.quality + 1
+
+      if (this.name == 'Backstage passes to a TAFKAL80ETC concert'
+          && this.quality < 50) {
+
+        const qualityDiff = this.sellIn < 6 ? 2 : 1
+
+        this.quality = this.quality + qualityDiff
+
+      }
+    }
+
+    this.sellIn = this.sellIn - 1;
+
+    if (this.sellIn < 0) {
+      if (this.name == 'Aged Brie') {
+        if (this.quality < 50) {
+          this.quality = this.quality + 1
+        }
+      } else if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        this.quality = 0
+      }
+    }
+  }
 }
 
 export class GildedRose {
@@ -20,35 +52,7 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
 
-      if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') {
-        continue;
-      }
-
-      if (this.items[i].quality < 50) {
-
-        this.items[i].quality = this.items[i].quality + 1
-
-        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
-            && this.items[i].quality < 50) {
-
-          const qualityDiff = this.items[i].sellIn < 6 ? 2 : 1
-
-          this.items[i].quality = this.items[i].quality + qualityDiff
-
-        }
-      }
-
-      this.items[i].sellIn = this.items[i].sellIn - 1;
-
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name == 'Aged Brie') {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          this.items[i].quality = 0
-        }
-      }
+      this.items[i].update()
     }
 
     return this.items;
