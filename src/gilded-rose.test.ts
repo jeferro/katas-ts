33d041b1e02017e0045c0364ebf73df5e84c4039
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-02 16:12:50 +0100

    refactor: remove ItemCreator and move creator to test

--- src/gilded-rose.test.ts
+++ src/gilded-rose.test.ts
@@ -1,12 +1,30 @@
 import { describe, expect } from 'vitest'
 import {GildedRose} from "./gilded-rose";
-import {ItemCreator} from "./items/ItemCreator";
+import {Item} from "./items/Item";
+import {Sulfuras} from "./items/Sulfuras";
+import {AgedBrie} from "./items/AgedBrie";
+import {BackstagePasses} from "./items/BackstagePasses";
 
 function cartesianProduct<T extends any[][]>(...arrays: T): any[][] {
     return arrays.reduce(
         (acc, curr) => acc.flatMap(combo => curr.map(item => [...combo, item])),
         [[]]
     );
+}
+
+function createItem(name: string, sellIn: number, quality: number): Item {
+  if(name === 'Sulfuras, Hand of Ragnaros'){
+    return new Sulfuras(sellIn, quality)
+  }
+  else if(name === 'Aged Brie'){
+    return new AgedBrie(sellIn, quality)
+  }
+  else if(name === 'Backstage passes to a TAFKAL80ETC concert'){
+    return new BackstagePasses(sellIn, quality)
+  }
+  else {
+    throw new Error(`Unknown name ${name}`)
+  }
 }
 
 describe('Gilded Rose', () => {
@@ -21,7 +39,7 @@ describe('Gilded Rose', () => {
         'golden test: name:%s sellIn:%s quality:%s',
         (name: string, sellIn: number, quality) => {
             const items = [
-                ItemCreator.create(name, sellIn, quality),
+                createItem(name, sellIn, quality),
             ]
             const gildedRose = new GildedRose(items)
 
