3cc4db6a556bbcdd5e5731c8f9fc609bfb5883c1
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-02 15:57:26 +0100

    refactor: remove dead code in method update in BackstagePasses

--- src/items/BackstagePasses.ts
+++ src/items/BackstagePasses.ts
@@ -8,16 +8,12 @@ export class BackstagePasses extends Item {
   }
 
   update(): void {
-    if (this.name === 'Sulfuras, Hand of Ragnaros') {
-      return;
-    }
 
     if (this.quality < 50) {
 
       this.quality = this.quality + 1
 
-      if (this.name == 'Backstage passes to a TAFKAL80ETC concert'
-          && this.quality < 50) {
+      if (this.quality < 50) {
 
         const qualityDiff = this.sellIn < 6 ? 2 : 1
 
@@ -29,13 +25,7 @@ export class BackstagePasses extends Item {
     this.sellIn = this.sellIn - 1;
 
     if (this.sellIn < 0) {
-      if (this.name == 'Aged Brie') {
-        if (this.quality < 50) {
-          this.quality = this.quality + 1
-        }
-      } else if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-        this.quality = 0
-      }
+      this.quality = 0
     }
   }
 }
