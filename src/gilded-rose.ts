03436ce648be792efb503ab92183b385b721920e
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-03 10:05:40 +0100

    refactor: clean code in method updateQualitySulfuras

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -60,49 +60,7 @@ export class Item {
   }
 
   private updateQualitySulfuras() {
-    const name = "Sulfuras, Hand of Ragnaros"
 
-    if (name != 'Aged Brie' && name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (name != 'Sulfuras, Hand of Ragnaros') {
-        if (this.quality > 0) {
-          this.quality = this.quality - 1
-        }
-      }
-    } else {
-      if (this.quality < 50) {
-        this.quality = this.quality + 1
-
-        if (name == 'Backstage passes to a TAFKAL80ETC concert') {
-
-          if (this.quality < 50) {
-            if (this.sellIn < 11) {
-              this.quality = this.quality + 1
-            }
-            if (this.sellIn < 6) {
-              this.quality = this.quality + 1
-            }
-          }
-        }
-      }
-    }
-
-    if (name != 'Sulfuras, Hand of Ragnaros') {
-      this.sellIn = this.sellIn - 1;
-    }
-
-    if (this.sellIn < 0) {
-      if (name == 'Aged Brie') {
-        if (this.quality < 50) {
-          this.quality = this.quality + 1
-        }
-      } else if (name == 'Backstage passes to a TAFKAL80ETC concert') {
-        this.quality = 0
-      } else if (name != 'Sulfuras, Hand of Ragnaros') {
-        if (this.quality > 0) {
-          this.quality = this.quality - 1
-        }
-      }
-    }
   }
 
   private updateQuality(name: string) {
