a1005037900eb7eed0eeb44a2a0b1ae1a67e8bea
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-03 07:35:54 +0100

    test: Support other item decrease quality by 2 once the sell by date has passed

--- src/gilded-rose.test.ts
+++ src/gilded-rose.test.ts
@@ -73,6 +73,17 @@ describe('Gilded Rose', () => {
         expect(items[0].quality).toBe(19)
     })
 
+    it('Support other item decrease quality by 2 once the sell by date has passed', () => {
+        const items = [
+            new Item('Other', 0, 20),
+        ]
+
+        const gildedRose = new GildedRose(items)
+        gildedRose.updateQuality()
+
+        expect(items[0].quality).toBe(18)
+    })
+
     it('Backstage passes Quality drops to 0 after the concert', () => {
         const items = [
             new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20),
