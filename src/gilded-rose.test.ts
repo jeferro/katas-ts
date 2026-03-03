1b78cdf6e833f81f8d7b7a139f3d9ff70ab8011f
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-03 07:17:31 +0100

    test: The Quality of an item is never more than 50

--- src/gilded-rose.test.ts
+++ src/gilded-rose.test.ts
@@ -1,11 +1,20 @@
 import { describe, it, expect } from 'vitest'
-import {GildedRose} from "./gilded-rose"
+import {GildedRose, Item} from "./gilded-rose"
 
 describe('Gilded Rose', () => {
 
-    it('should create instance', () => {
-        const gildedRose = new GildedRose()
+    it('The Quality of an item is never more than 50', () => {
+        const items = [
+            new Item('Aged Brie', 10, 50),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 10, 50),
+            new Item('Sulfuras, Hand of Ragnaros', 10, 50)
+        ]
 
-        expect(gildedRose).not.toBeUndefined()
+        const gildedRose = new GildedRose(items)
+        gildedRose.updateQuality()
+
+        expect(items[0].quality).toBe(50)
+        expect(items[1].quality).toBe(50)
+        expect(items[2].quality).toBe(50)
     })
 })
