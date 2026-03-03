5f969b0dac3e6fdcd34ef0acbc5584f614b0e85b
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-03 09:52:52 +0100

    refactor: create method updateQualityOfAgedBrie

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -12,7 +12,7 @@ export class Item {
   public updateQualityOfItem(name: string = this.name) {
     switch (name){
       case 'Aged Brie':
-        this.updateQuality('Aged Brie')
+        this.updateQualityOfAgedBrie('Aged Brie')
         break
       case 'Sulfuras, Hand of Ragnaros':
         this.updateQuality('Sulfuras, Hand of Ragnaros')
@@ -22,6 +22,50 @@ export class Item {
         break
       default:
         this.updateQuality(name)
+    }
+  }
+
+  private updateQualityOfAgedBrie(name: string) {
+    if (name != 'Aged Brie' && name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (name != 'Sulfuras, Hand of Ragnaros') {
+        if (this.quality > 0) {
+          this.quality = this.quality - 1
+        }
+      }
+    } else {
+      if (this.quality < 50) {
+        this.quality = this.quality + 1
+
+        if (name == 'Backstage passes to a TAFKAL80ETC concert') {
+
+          if (this.quality < 50) {
+            if (this.sellIn < 11) {
+              this.quality = this.quality + 1
+            }
+            if (this.sellIn < 6) {
+              this.quality = this.quality + 1
+            }
+          }
+        }
+      }
+    }
+
+    if (name != 'Sulfuras, Hand of Ragnaros') {
+      this.sellIn = this.sellIn - 1;
+    }
+
+    if (this.sellIn < 0) {
+      if (name == 'Aged Brie') {
+        if (this.quality < 50) {
+          this.quality = this.quality + 1
+        }
+      } else if (name == 'Backstage passes to a TAFKAL80ETC concert') {
+        this.quality = 0
+      } else if (name != 'Sulfuras, Hand of Ragnaros') {
+        if (this.quality > 0) {
+          this.quality = this.quality - 1
+        }
+      }
     }
   }
 
