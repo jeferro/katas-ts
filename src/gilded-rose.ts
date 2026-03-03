b1161548fb0e6ad78848547650e38e4c7ed04f45
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-03 10:14:20 +0100

    refactor: extract methods decrementSellIn and incrementQuality

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -22,15 +22,12 @@ export class Item {
   }
 
   private updateQualityOfAgedBrie() {
-    if (this.quality < 50) {
-      this.quality = this.quality + 1
-    }
+    this.incrementQuality()
 
-    this.sellIn = this.sellIn - 1;
+    this.decrementSellIn();
 
-    if (this.sellIn < 0 && this.quality < 50) {
-      this.quality = this.quality + 1
-
+    if (this.sellIn < 0) {
+      this.incrementQuality();
     }
   }
 
@@ -71,6 +68,16 @@ export class Item {
         this.quality = this.quality - 1
       }
 
+    }
+  }
+
+  private decrementSellIn() {
+    this.sellIn = this.sellIn - 1;
+  }
+
+  private incrementQuality() {
+    if (this.quality < 50) {
+      this.quality = this.quality + 1
     }
   }
 }
