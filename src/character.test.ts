30a7cfa4a85ab9732b2a6f00ba9da136f4d6a6ca
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-04 23:19:05 +0100

    feature: should create starting health at 1000 and alive

--- src/character.test.ts
+++ src/character.test.ts
@@ -4,9 +4,13 @@ import {Character} from "./character"
 
 describe('Character', () => {
 
-    it('should create instance', () => {
-        const character = new Character()
+    it('should create starting health at 1000 and alive', () => {
+        const character = Character.create()
 
-        expect(character).not.toBeUndefined()
+        expect(character.health).toBe(1000)
+        expect(character.isAlive).toBeTruthy()
+        expect(character.isDead).toBeFalsy()
     })
+
+
 })
--- src/character.ts
+++ src/character.ts
@@ -1,4 +1,16 @@
 
 export class Character {
+  readonly health: number = 1000
 
+  static create(): Character {
+    return new Character()
+  }
+
+  public get isAlive() {
+    return this.health > 0
+  }
+
+  public get isDead() {
+    return !this.isAlive
+  }
 }
