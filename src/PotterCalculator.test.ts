3dba3ae01fe2e469076b5ff75c7316f83d368466
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-08 01:07:31 +0100

    feature: return simple discounts

--- src/PotterCalculator.test.ts
+++ src/PotterCalculator.test.ts
@@ -5,13 +5,20 @@ describe('PotterCalculator', () => {
 
     const potterCalculator = new PotterCalculator()
 
-    it('should calculate basics', () => {
+    it('should calculate basic price', () => {
         expect(potterCalculator.calculate([])).toBe(0)
+        expect(potterCalculator.calculate([0])).toBe(8)
         expect(potterCalculator.calculate([1])).toBe(8)
         expect(potterCalculator.calculate([2])).toBe(8)
         expect(potterCalculator.calculate([3])).toBe(8)
         expect(potterCalculator.calculate([4])).toBe(8)
-        expect(potterCalculator.calculate([5])).toBe(8)
-        expect(potterCalculator.calculate([1, 1, 1])).toBe(8 * 3)
+        expect(potterCalculator.calculate([0, 0, 0])).toBe(8 * 3)
+    })
+
+    it('should calculate simple discount', () => {
+        expect(potterCalculator.calculate([0, 1])).toBe(8 * 2 * 0.95)
+        expect(potterCalculator.calculate([0, 1, 2])).toBe(8 * 3 * 0.9)
+        expect(potterCalculator.calculate([0, 1, 2, 3])).toBe(8 * 4 * 0.8)
+        expect(potterCalculator.calculate([0, 1, 2, 3, 4])).toBe(8 * 5 * 0.75)
     })
 })
--- src/PotterCalculator.ts
+++ src/PotterCalculator.ts
@@ -1,7 +1,68 @@
 
 export class PotterCalculator {
 
+  private readonly PRICE_BOOK = 8;
+
   calculate(books: number[]): number {
-    return 8 * books.length
+    if(books.length === 0) {
+      return 0
+    }
+
+    if(books.length === 1) {
+      return this.PRICE_BOOK
+    }
+
+    const numberByBooks = this.calculateNumberByBooks(books)
+
+    let totalPrice = 0
+    let sizePack: number
+
+    do {
+      sizePack = 0
+
+      for(let i = 0; i < numberByBooks.length; i++) {
+        if(numberByBooks[i] === 0) {
+          continue
+        }
+
+        numberByBooks[i] = numberByBooks[i] - 1
+
+        sizePack++
+      }
+
+      let discount = this.calculateDiscount(sizePack)
+
+      totalPrice += (sizePack * this.PRICE_BOOK) * discount
+    } while (sizePack !== 0)
+
+    return totalPrice
+  }
+
+  private calculateDiscount(sizePack: number) {
+    if(sizePack === 0 || sizePack === 1) {
+      return 1
+    }
+
+    if(sizePack === 2) {
+      return 0.95
+    }
+
+    if(sizePack === 3) {
+      return 0.9
+    }
+
+    if(sizePack === 4) {
+      return 0.8
+    }
+
+    return 0.75
+  }
+
+  private calculateNumberByBooks(books: number[]): number[] {
+    let numberByBooks = [0, 0, 0, 0, 0]
+
+    books.forEach(book => numberByBooks[book]++)
+
+    return numberByBooks
   }
 }
