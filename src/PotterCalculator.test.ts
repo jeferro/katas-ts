b7ba34fe361c0a48c2369b0a9b2fa687a2065b32
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-08 01:09:27 +0100

    feature: return several discounts

--- src/PotterCalculator.test.ts
+++ src/PotterCalculator.test.ts
@@ -21,4 +21,11 @@ describe('PotterCalculator', () => {
         expect(potterCalculator.calculate([0, 1, 2, 3])).toBe(8 * 4 * 0.8)
         expect(potterCalculator.calculate([0, 1, 2, 3, 4])).toBe(8 * 5 * 0.75)
     })
+
+    it('should calculate several discount', () => {
+        expect(potterCalculator.calculate([0, 0, 1])).toBe(8 + (8 * 2 * 0.95))
+        expect(potterCalculator.calculate([0, 0, 1, 1])).toBe(2 * (8 * 2 * 0.95))
+        expect(potterCalculator.calculate([0, 0, 1, 2, 2, 3])).toBe((8 * 4 * 0.8) + (8 * 2 * 0.95))
+        expect(potterCalculator.calculate([0, 1, 1, 2, 3, 4])).toBe(8 + (8 * 5 * 0.75))
+    })
 })
