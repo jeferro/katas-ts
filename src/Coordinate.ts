f6550ff41dc89b25f9bbe3152d3e0f9207936148
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-08 22:49:18 +0100

    feature: create class Coordinate

---
+++ src/Coordinate.ts
@@ -0,0 +1,14 @@
+export class Coordinate {
+  public readonly x: number;
+
+  public readonly y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+
+  static create(x: number, y: number): Coordinate {
+    return new Coordinate(x, y)
+  }
+}
