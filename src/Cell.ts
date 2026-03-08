26a6ae7737c00641a421f3c0c5770c1fca038b12
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-08 22:06:12 +0100

    feature: create class Cell

---
+++ src/Cell.ts
@@ -0,0 +1,23 @@
+export class Cell {
+  private _value: string | undefined;
+
+  private _marked: boolean;
+
+  constructor(value: string | undefined,
+              marked: boolean) {
+    this._value = value
+    this._marked = marked
+  }
+
+  static createEmpty(): Cell {
+    return new Cell(undefined, false)
+  }
+
+  public get value(): string | undefined {
+    return this._value
+  }
+
+  public get marked(): boolean {
+    return this._marked
+  }
+}
