6f530ea8441b1d16b4032b1441c895b6187868b0
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-09 08:16:49 +0100

    tests: increase branch coverage to 100%

--- src/StatementPrinter.test.ts
+++ src/StatementPrinter.test.ts
@@ -8,11 +8,16 @@ import {Play} from "./Play"
 describe('StatementPrinter', () => {
 
   const plays = {
-    "hamlet": new Play("Hamlet", "tragedy")
+    "hamlet": new Play("Hamlet", "tragedy"),
+    "earnest": new Play("The Importance of Being Earnest", "comedy"),
+    "salesman": new Play("Death of a Salesman", "drama")
   }
 
   const invoices = [
-    new Invoice("customer-test-1", [new Performance("hamlet", 100)])
+    new Invoice("customer-test-1", [new Performance("hamlet", 100)]),
+    new Invoice("customer-test-2", [new Performance("hamlet", 30)]),
+    new Invoice("customer-test-3", [new Performance("earnest", 100)]),
+    new Invoice("customer-test-4", [new Performance("earnest", 20)])
   ]
 
   const statementPrinter = new StatementPrinter()
@@ -21,5 +26,11 @@ describe('StatementPrinter', () => {
     const result = statementPrinter.print(invoice, plays)
 
     expect(result).toMatchSnapshot()
+  })
+
+  it('should fail when type is unknown', () => {
+    const invoice = new Invoice("customer-fail-type", [new Performance("salesman", 100)])
+
+    expect(() => statementPrinter.print(invoice, plays)).toThrowError()
   })
 })
