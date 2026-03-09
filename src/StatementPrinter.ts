7275744809be669c9eed5147a3dc4d47ab95b6a7
Juan M. Estévez Ferro <jeferro@gmail.com>
2026-03-09 09:57:42 +0100

    refactor: convert variable format to class constant

--- src/StatementPrinter.ts
+++ src/StatementPrinter.ts
@@ -3,32 +3,33 @@ import { Play } from "./Play";
 import { Performance } from "./Performance";
 
 export class StatementPrinter {
+  private readonly format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
   print(invoice: Invoice, plays: Record<string, Play>): string {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\r\n\r\n`;
-    const format = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format;
 
     for (const perf of invoice.performances) {
       const play = plays[perf.playID];
       let audience = perf.audience;
 
       let thisAmount = play.calculateAmount(audience);
+      totalAmount += thisAmount;
 
       let credits = play.calculateCredits(audience);
-
       volumeCredits += credits
 
-      result += `${play.name}: ${format(thisAmount / 100)} (${audience} seats)\r\n`;
-      totalAmount += thisAmount;
+      result += `${play.name}: ${this.format(thisAmount / 100)} (${audience} seats)\r\n`;
     }
 
-    result += `\r\nAmount owed is ${format(totalAmount / 100)}\r\n`;
+    result += `\r\nAmount owed is ${this.format(totalAmount / 100)}\r\n`;
     result += `You earned ${volumeCredits} credits\r\n\r\n`;
+
     return result;
   }
 }
